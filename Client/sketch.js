ae19885f342a94545240af3610f9b28b6ec217e2
gkaragoz <annotto3@gmail.com>
2017-09-30 17:25:40 +0300

    Add mouseDragged event which draws something into canvas.

--- Client/sketch.js
+++ Client/sketch.js
@@ -1,12 +1,42 @@
 var canvas;
 
+var pencilColor;
+var pencilSize;
+
+var mouseX;
+var mouseY;
+
+var sketchData = {
+    x: mouseX,
+    y: mouseY
+};
+
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0,0);
+    
+    background(51);
+    
+    pencilColor = 255;
+    pencilSize = 36;
 }
 
 function draw() {
-    background(51);
+    
+}
+
+function mouseDragged() {
+    this.mouseX = mouseX;
+    this.mouseY = mouseY;
+    
+    noStroke();
+    fill(pencilColor);
+    ellipse(this.mouseX, this.mouseY, pencilSize, pencilSize);
+    
+    sketchData.x = this.mouseX;
+    sketchData.y = this.mouseY;
+    
+    console.log(sketchData);
 }
 
 function windowResized() {
