11310a20bcc1c98998f54439341357b8223f5e94
gkaragoz <gkaragoz96@gmail.com>
2017-11-25 16:38:20 +0300

    Refactor sketch.js make paint method

--- Client/sketch.js
+++ Client/sketch.js
@@ -9,28 +9,38 @@ function setup() {
 }
 
 function draw() {
-	noStroke();
-
-  //The distance between pencil and mouse coordinates
-	var distance = dist(pencilX, pencilY, mouseX, mouseY);
-
-	for (var ii=0; ii<=distance; ii+=10) {
-    //Lerp it between pencilX and mouseX
-		var nx = lerp(pencilX, mouseX, ii/distance);
-		var ny = lerp(pencilY, mouseY, ii/distance);
-
-    //Make it colorful, otherwise its blue
-		fill(255*ii/distance, 255, 255);
-
-    //Draw lerped dots
-		ellipse(nx, ny, 10, 10);
-	}
-
-  //Draw real impulsed dots by frameRate
-	fill(255);
-	ellipse(mouseX, mouseY, 20, 20);
-
   //Always get mousePosition and set it to pencil coordinates
 	pencilX = mouseX;
 	pencilY = mouseY;
+}
+
+function mousePressed() {
+    paint();
+}
+
+function mouseDragged() {
+    paint();
+}
+
+function paint() {
+  noStroke();
+
+  //The distance between pencil and mouse coordinates
+  var distance = dist(pencilX, pencilY, mouseX, mouseY);
+
+  for (var ii=0; ii<=distance; ii+=10) {
+    //Lerp it between pencilX and mouseX
+    var nx = lerp(pencilX, mouseX, ii/distance);
+    var ny = lerp(pencilY, mouseY, ii/distance);
+
+    //Make it colorful, otherwise its blue
+    fill(255*ii/distance, 255, 255);
+
+    //Draw lerped dots
+    ellipse(nx, ny, 10, 10);
+  }
+
+  //Draw real impulsed dots by frameRate
+  fill(255);
+  ellipse(mouseX, mouseY, 20, 20);
 }
