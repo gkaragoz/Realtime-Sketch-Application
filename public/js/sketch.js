f3a937b0d1f9dd5a3d0f7395f4e5b752ee0587fc
oguzhanaydemir <ouzhanaydemir7@gmail.com>
2017-12-29 02:34:49 +0300

    First prototype of button UI into canvas

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,12 +1,17 @@
 var pencil;
 
 function setup() {
-	var canvas = createCanvas(885, windowHeight);
+  	var canvas = createCanvas(885, windowHeight);
 	
 	background(0);
   colorMode(HSB);
   canvas.parent('draw_main');
   
+  button = createButton('click me');
+  button.parent('draw_main');
+  button.id('btn_color_01');
+  button.position(50, windowHeight - 50);
+  button.mousePressed(buttonClick);
 
   //Create a new Instance of pencil
   pencil = new pencil(mouseX, mouseY);
@@ -24,4 +29,8 @@ function mousePressed() {
 
 function mouseDragged() {
     pencil.paint(mouseX, mouseY);
+}
+
+function buttonClick() {
+  console.log("Button clicked");
 }
